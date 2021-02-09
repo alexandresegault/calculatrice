cc657d6f626979b32343de7213a0cd9bb5775ac1
alexandresegault <alexandresegault@gmail.com>
2021-02-09 01:24:18 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import Calculatrice from "./Components/Calculatrice";
+function App() {
+  return (
+    <div className="App">
+      <Calculatrice />
+    </div>
+  );
+}
+
+export default App;
